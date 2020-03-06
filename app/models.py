e3ffddd4bb4cb05e3f514bd66ec6f2b0a7e18d4b
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-03-06 12:42:27 -0600

    logs stuff

--- app/models.py
+++ app/models.py
@@ -4,39 +4,28 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db
 
-
-class Message(db.Model):
-    __tablename__ = 'Message'
-
-    MessageSid = db.Column(db.String, primary_key=True)
-    AccountSid = db.Column(db.String)
-    ApiVersion = db.Column(db.String)
-    Body = db.Column(db.String)
-    From = db.Column(db.String)
-    To = db.Column(db.String)
-    MediaUrl = db.Column(db.String, nullable=True)
-    DateReceived = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __init__(self, MessageSid, AccountSid, ApiVersion,
-                 Body, From, To, MediaUrl):
-        self.MessageSid = MessageSid
-        self.AccountSid = AccountSid
-        self.ApiVersion = ApiVersion
-        self.Body = Body
-        self.From = From
-        self.To = To
-        self.MediaUrl = MediaUrl
+class SerializableModel(object):
+    def to_dict(self):
+        value = {}
+        for column in self.__table__.columns:
+            attribute = getattr(self, column.name)
+            if isinstance(attribute, datetime):
+                attribute = str(attribute)
+            value[column.name] = attribute
+        return value
 
 
-class Phonebook(db.Model):
-    __tablename__ = 'Phonebook'
+class Log(db.Model, SerializableModel):
+    __tablename__ = 'logfts'
 
-    Name = db.Column(db.String, primary_key=True)
-    Number = db.Column(db.String)
-
-    def __init__(self, name, number):
-        self.Name = name
-        self.Number = number
+    time = db.Column(db.Text)
+    server = db.Column(db.Text)
+    chan = db.Column(db.Text)
+    nick = db.Column(db.Text, primary_key=True)
+    user = db.Column(db.Text)
+    action = db.Column(db.Text)
+    msg = db.Column(db.Text, primary_key=True)
+    uts = db.Column(db.Text, primary_key=True)
 
 
 class User(UserMixin, db.Model):
--- app/views.py
+++ app/views.py
@@ -1,68 +1,51 @@
-from flask import render_template, flash, redirect, url_for, Blueprint
-from flask_login import current_user, login_required
+from datetime import datetime
+from flask import abort, render_template, Blueprint, Markup
+from flask_login import login_required
+from re import sub, IGNORECASE
 
-from app import app, db, login
-from app.models import Message, Phonebook
+from app import app, db
+from app.models import Log
 
 
 bp = Blueprint('logs', __name__, url_prefix='/logviewer')
 
 
-@bp.route('/', defaults={'name': None}, methods=['GET'])
-@bp.route('/<name>', methods=['GET'])
+@bp.route('/', defaults={'chan': None, 'date': None}, methods=['GET'])
+@bp.route('/<chan>/<date>', methods=['GET'])
 @login_required
-def index(name):
-    if name:
-        messages = db.session.query(Message, Phonebook) \
-            .outerjoin(Phonebook, Phonebook.Number==Message.From) \
-            .filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']) \
-            .filter(Message.To==app.config['PHONE_NUMBER']) \
-            .filter(Message.MediaUrl!=None) \
-            .filter(Phonebook.Name==name) \
-            .order_by(Message.DateReceived.desc()) \
+def index(chan, date):
+    formats = {
+        'PRIVMSG': '{time} < {nick}> {msg}',
+        'PART': '{time} -!- {nick} [{user}] has left {chan} [{msg}]',
+        'JOIN': '{time} -!- {nick} [{user}] has joined {chan}',
+        'MODE': '{time} -!- mode/{chan} [{msg}] by {nick}',
+        'KICK': '{time} -!- {nick} was kicked from {chan} by {msg}',
+        'TOPIC': '{time} -!- {nick} changed the topic of {chan} to: {msg}',
+        'QUIT': '{time} -!- {nick} has quit IRC [{msg}]',
+        'NICK': '{time} -!- {nick} [{user}] is now known as {msg}',
+    }
+
+    if chan == date:
+        return render_template('index.html', logs=[])
+
+    try:
+        datetime.strptime(date, "%Y-%m-%d")  # input validation
+
+        logs = db.session.query(Log) \
+            .filter(db.text(f"logfts MATCH 'chan:\"{chan}\" AND time:\"{date}\"'")) \
             .all()
-    else:
-        messages = db.session.query(Message, Phonebook) \
-            .outerjoin(Phonebook, Phonebook.Number==Message.From) \
-            .filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']) \
-            .filter(Message.To==app.config['PHONE_NUMBER']) \
-            .filter(Message.MediaUrl!=None) \
-            .filter(Phonebook.Name!=None) \
-            .order_by(Message.DateReceived.desc()) \
-            .all()
 
-    phonebook = db.session.query(Phonebook) \
-        .outerjoin(Message, Phonebook.Number==Message.From) \
-        .filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']) \
-        .filter(Message.To==app.config['PHONE_NUMBER']) \
-        .filter(Message.MediaUrl!=None) \
-        .filter(Phonebook.Name!=None) \
-        .order_by(Phonebook.Name.asc()) \
-        .distinct(Phonebook.Name)
+        for line in logs:
+            line.time = line.time[11:]
+            line.msg = str(Markup.escape(line.msg))
+            line.msg = sub(r'(https?:\/\/(?:www\.)?([^: \/]+\.[^: \/]+)(?::\d+)?\/?\S*)',
+                           r'<a href="\1">\1</a>', line.msg, flags=IGNORECASE)
 
-    return render_template('index.html', msgs=messages, pb=phonebook)
+        logs = [Markup(formats[line.action].format(**line.to_dict()))
+                for line in logs if line.action not in ['PING', 'NOTICE']]
 
+        return render_template('index.html', logs=logs)
+    except Exception as ex:
+        abort(400, ex)
 
-@bp.route('/image/<sid>', methods=['GET'])
-@login_required
-def image(sid):
-    messages = db.session.query(Message, Phonebook) \
-        .outerjoin(Phonebook, Phonebook.Number==Message.From) \
-        .filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']) \
-        .filter(Message.To==app.config['PHONE_NUMBER']) \
-        .filter(Message.MediaUrl!=None) \
-        .filter(Phonebook.Name!=None) \
-        .filter(Message.MessageSid==sid) \
-        .all()
-
-    phonebook = db.session.query(Phonebook) \
-        .outerjoin(Message, Phonebook.Number==Message.From) \
-        .filter(Message.AccountSid==app.config['TWILIO_ACCOUNT_SIDS_CSV']) \
-        .filter(Message.To==app.config['PHONE_NUMBER']) \
-        .filter(Message.MediaUrl!=None) \
-        .filter(Phonebook.Name!=None) \
-        .order_by(Phonebook.Name.asc()) \
-        .distinct(Phonebook.Name)
-
-    return render_template('image.html', msgs=messages, pb=phonebook)
 
