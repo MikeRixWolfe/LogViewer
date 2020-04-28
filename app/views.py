235c75f1d6010d04729c11862d1ea9d0b055501e
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-04-28 12:17:30 -0500

    better irc_color_re

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ from app.util import color_hash
 bp = Blueprint('logs', __name__, url_prefix='/logviewer')
 
 url_re = compile(r'(https?:\/\/(?:www\.)?([^: \/]+\.[^: \/]+)(?::\d+)?\/?[^\" ]*)', IGNORECASE)
-irc_color_re = compile(r'(\x03(\d{1,2}(,\d{1,2})?)|[\x0f\x02\x16\x1f])')
+irc_color_re = compile(r'(\x03(\d{1,2}(,\d{1,2})?)|[\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f])')
 
 
 @bp.route('/', methods=['GET'])
