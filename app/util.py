737f619f7c3a5c727f3d56f6b6fc11434b178fc9
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-11-09 21:02:46 -0600

    refactoring

--- app/util.py
+++ app/util.py
@@ -1,4 +1,45 @@
+from flask import Markup
+from re import compile, sub, IGNORECASE
+from time import localtime, strftime
 from app import app
+
+
+url_re = compile(r'(https?:\/\/(?:www\.)?([^: \/]+\.[^: \/]+)(?::\d+)?\/?[^\" ]*)', IGNORECASE)
+irc_color_re = compile(r'(\x03(\d{1,2}(,\d{1,2})?)|[\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f])')
+formats = {
+    'PRIVMSG': u'{time} < <span style="color: {color}">{nick}</span>> {msg}',
+    'ACTION': u'{time} {msg}',
+    'PART': u'{time} -!- {nick} [{user}] has left {chan} [{msg}]',
+    'JOIN': u'{time} -!- {nick} [{user}] has joined {chan}',
+    'MODE': u'{time} -!- mode/{chan} [{msg}] by {nick}',
+    'KICK': u'{time} -!- {who} was kicked from {chan} by {nick} with reason {msg}',
+    'TOPIC': u'{time} -!- {nick} changed the topic of {chan} to: {msg}',
+    'QUIT': u'{time} -!- {nick} has quit IRC [{msg}]',
+    'NICK': u'{time} -!- {nick} [{user}] is now known as {msg}',
+}
+
+
+def format_line(line):
+    line['link'] = '/logviewer/{}/{}/{}'.format(line['chan'].strip('#'), *line['time'].split())
+    line['time'] = line['time'][11:]
+
+    line['msg'] = irc_color_re.sub('', line['msg'])
+    line['msg'] = str(Markup.escape(line['msg'].encode('ascii', 'ignore')))
+    line['msg'] = url_re.sub(r'<a href="\1">\1</a>', line['msg'])
+
+    if line['action'] == 'KICK':
+        line['who'], line['msg'] = line['msg'].split(' ', 1)
+
+    line['msg'] = Markup(formats[line['action']].format(color=color_hash(line['nick']), **line))
+
+    return line
+
+
+def format_quote(quote):
+    quote['date'] = "{} {}".format("Early" if int(strftime("%d", localtime(float(quote['uts'])))) < 15 else "Late",
+        strftime("%b %Y", localtime(float(quote['uts']))))
+
+    return quote
 
 
 def color_hash(text):
@@ -7,3 +48,4 @@ def color_hash(text):
               'rgb(0,187,187)', 'rgb(85,255,255)', 'rgb(85,85,252)', 'rgb(255,85,255)',
               'rgb(85,85,85)']
     return colors[sum(ord(c) for c in text.lower()) % 11]
+
--- app/views.py
+++ app/views.py
@@ -1,31 +1,15 @@
 from datetime import datetime
-from flask import abort, redirect, render_template, url_for, Blueprint, Markup
+from flask import abort, redirect, render_template, url_for, Blueprint
 from flask_login import login_required
-from re import compile, sub, IGNORECASE
-from time import localtime, strftime
 
 from app import app, db
 from app.forms import SearchForm
 from app.models import Log, Quote
 from app.tokenize import build_query
-from app.util import color_hash
+from app.util import color_hash, format_line, format_quote
 
 
 bp = Blueprint('logs', __name__, url_prefix='/logviewer')
-
-url_re = compile(r'(https?:\/\/(?:www\.)?([^: \/]+\.[^: \/]+)(?::\d+)?\/?[^\" ]*)', IGNORECASE)
-irc_color_re = compile(r'(\x03(\d{1,2}(,\d{1,2})?)|[\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f])')
-formats = {
-    'PRIVMSG': u'{time} < <span style="color: {color}">{nick}</span>> {msg}',
-    'ACTION': u'{time} {msg}',
-    'PART': u'{time} -!- {nick} [{user}] has left {chan} [{msg}]',
-    'JOIN': u'{time} -!- {nick} [{user}] has joined {chan}',
-    'MODE': u'{time} -!- mode/{chan} [{msg}] by {nick}',
-    'KICK': u'{time} -!- {who} was kicked from {chan} by {nick} with reason {msg}',
-    'TOPIC': u'{time} -!- {nick} changed the topic of {chan} to: {msg}',
-    'QUIT': u'{time} -!- {nick} has quit IRC [{msg}]',
-    'NICK': u'{time} -!- {nick} [{user}] is now known as {msg}',
-}
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -42,21 +26,8 @@ def search():
                 .filter(db.text("logfts MATCH '{}'".format(build_query(form.search.data.replace("'", "''").replace('"', '""'))))) \
                 .order_by(db.desc(db.cast(Log.uts, db.Float))).limit(100).all()
 
-            logs = [line.to_dict() for line in logs]
-
-            for line in logs:
-                line['link'] = '/logviewer/{}/{}/{}'.format(line['chan'].strip('#'), *line['time'].split())
-
-                line['msg'] = irc_color_re.sub('', line['msg'])
-                line['msg'] = str(Markup.escape(line['msg'].encode('ascii', 'ignore')))
-                line['msg'] = url_re.sub(r'<a href="\1">\1</a>', line['msg'])
-
-                if line['action'] == 'KICK':
-                    line['who'], line['msg'] = line['msg'].split(' ', 1)
-
-                line['msg'] = Markup(formats[line['action']].format(color=color_hash(line['nick']), **line))
-
-            logs = [line for line in logs if line['action'] not in ['PING', 'NOTICE']]
+            logs = [format_line(line.to_dict()) for line in logs
+                if line.action not in ['PING', 'NOTICE']]
 
         return render_template('search.html', form=form, logs=logs, query=query)
     except Exception as ex:
@@ -76,20 +47,8 @@ def index(chan, date, time):
             .filter(db.text("logfts MATCH '(chan:\"#{}\" OR chan:\"nick\" OR chan:\"quit\") AND time:\"{}\"'".format(chan, date))) \
             .all()
 
-        logs = [line.to_dict() for line in logs]
-
-        for line in logs:
-            line['time'] = line['time'][11:]
-
-            line['msg'] = irc_color_re.sub('', line['msg'])
-            line['msg'] = str(Markup.escape(line['msg'].encode('ascii', 'ignore')))
-            line['msg'] = url_re.sub(r'<a href="\1">\1</a>', line['msg'])
-
-            if line['action'] == 'KICK':
-                line['who'], line['msg'] = line['msg'].split(' ', 1)
-
-        logs = [Markup(formats[line['action']].format(color=color_hash(line['nick']),
-            **line)) for line in logs if line['action'] not in ['PING', 'NOTICE']]
+        logs = [format_line(line.to_dict()) for line in logs
+            if line.action not in ['PING', 'NOTICE']]
 
         return render_template('index.html', logs=logs, ts=time)
     except Exception as ex:
@@ -101,12 +60,7 @@ def index(chan, date, time):
 def quotes():
     try:
         quotes = db.session.query(Quote).filter(Quote.active == '1').all()
-        quotes = [quote.to_dict() for quote in quotes]
-
-        for quote in quotes:
-            quote['date'] = "{} {}".format(
-                "Early" if int(strftime("%d", localtime(float(quote['uts'])))) < 15 else "Late",
-                strftime("%b %Y", localtime(float(quote['uts']))))
+        quotes = [format_quote(quote.to_dict()) for quote in quotes]
 
         return render_template('quotes.html', quotes=quotes)
     except Exception as ex:
@@ -118,12 +72,7 @@ def quotes():
 def deleted_quotes():
     try:
         quotes = db.session.query(Quote).filter(Quote.active == '0').all()
-        quotes = [quote.to_dict() for quote in quotes]
-
-        for quote in quotes:
-            quote['date'] = "{} {}".format(
-                "Early" if int(strftime("%d", localtime(float(quote['uts'])))) < 15 else "Late",
-                strftime("%b %Y", localtime(float(quote['uts']))))
+        quotes = [format_quote(quote.to_dict()) for quote in quotes]
 
         return render_template('quotes.html', quotes=quotes)
     except Exception as ex:
@@ -135,12 +84,7 @@ def deleted_quotes():
 def all_quotes():
     try:
         quotes = db.session.query(Quote).all()
-        quotes = [quote.to_dict() for quote in quotes]
-
-        for quote in quotes:
-            quote['date'] = "{} {}".format(
-                "Early" if int(strftime("%d", localtime(float(quote['uts'])))) < 15 else "Late",
-                strftime("%b %Y", localtime(float(quote['uts']))))
+        quotes = [format_quote(quote.to_dict()) for quote in quotes]
 
         return render_template('quotes.html', quotes=quotes)
     except Exception as ex:
